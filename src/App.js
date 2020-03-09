bc6fab3184e9c1f17ffdb90b41aba23de16fce4e
Gloria Chen <cmxgloria@gmail.com>
2020-03-09 12:07:15 +1100

    updated some code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Recipe from "./Recipe";
+import { Link } from "react-router-dom";
 // import Recipes from "./Recipes";
 import "./App.css";
 
